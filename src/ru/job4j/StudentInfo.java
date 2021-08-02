4ea0c45ea11b9f1ca676ef622e9da8f93298c5f2
neggoGH <neggo1985@mail.ru>
2021-08-03 01:47:29 +0300

    3.3. Разбор первой программы. [#159033]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Sergey Ryazancev");
+        System.out.println("01.10.1987");
+    }
+}
