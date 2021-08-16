9fce82cbd7a046366c82706eb155cd857a3b2023
neggoGH <neggo1985@mail.ru>
2021-08-16 21:44:57 +0300

    6.0.1. Объявление массива. [#3613]

---
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,9 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        Short[] ages = new Short[10];
+        String[] surnames = new String[100500];
+        Float[] prices = new Float[40];
+    }
+}
