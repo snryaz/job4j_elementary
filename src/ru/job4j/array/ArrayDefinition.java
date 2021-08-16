9274e385af0cac349fd5b441a3eef461ffa289d0
neggoGH <neggo1985@mail.ru>
2021-08-16 21:47:53 +0300

    6.0.1.1. Размер массива [#333582]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,8 @@ public class ArrayDefinition {
         Short[] ages = new Short[10];
         String[] surnames = new String[100500];
         Float[] prices = new Float[40];
+        System.out.println("Размер массива равен: " + ages.length);
+        System.out.println("Размер массива равен: " + surnames.length);
+        System.out.println("Размер массива равен: " + prices.length);
     }
 }
