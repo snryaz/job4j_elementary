9d23af6de9d8f667f36d81dca990936ec581f6e3
neggoGH <neggo1985@mail.ru>
2021-08-18 20:57:04 +0300

    6.4. Слово начинается с ... [#41585]

---
+++ src/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    public static boolean startWidt(char[] word, char[] pref) {
+        for (int i = 0; i < pref.length; i++) {
+            if (pref[i] != word[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
