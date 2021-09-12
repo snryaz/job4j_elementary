060e378889433992049a466d8667f4f4b5bea228
neggoGH <neggo1985@mail.ru>
2021-09-12 23:40:45 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -6,6 +6,17 @@ public class Max {
         return compare ? left : right;
     }
 
+    public static int max(int first, int second, int third) {
+        return max(first,
+                max(second, third));
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        return max(first,
+                max(second,
+                        max(third, fourth)));
+    }
+
     public static void main(String[] args) {
         int result = Max.max(70, 100);
         System.out.println("наибольшее число: " + result);
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -33,4 +33,78 @@ public class MaxTest {
         int expected = 5;
         Assert.assertEquals(expected, result);
     }
+
+    @Test
+    public void when3ArgumentsThirdIsMore() {
+        int first = 1;
+        int second = 2;
+        int third = 3;
+        int result = Max.max(first, second, third);
+        int expected = 3;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void when3ArgumentsSecondIsMore() {
+        int first = 20;
+        int second = 33;
+        int third = 27;
+        int result = Max.max(first, second, third);
+        int expected = 33;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void when3ArgumentsFirstIsMore() {
+        int first = 120;
+        int second = 33;
+        int third = 27;
+        int result = Max.max(first, second, third);
+        int expected = 120;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void when4ArgumentsFourthIsMore() {
+        int first = 20;
+        int second = 33;
+        int third = 27;
+        int fourth = 45;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 45;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void when4ArgumentsThirdIsMore() {
+        int first = 20;
+        int second = 33;
+        int third = 127;
+        int fourth = 45;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 127;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void when4ArgumentsSecondIsMore() {
+        int first = 20;
+        int second = 133;
+        int third = 27;
+        int fourth = 45;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 133;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void when4ArgumentsFirstIsMore() {
+        int first = 120;
+        int second = 33;
+        int third = 27;
+        int fourth = 45;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 120;
+        Assert.assertEquals(expected, result);
+    }
 }
